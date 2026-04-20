29c6a575aad19964cc7a24b66ebbaf62b7d7b2fd
imyoonsoo <bliss38soo@gmail.com>
2026-04-20 16:55:00 +0900

    chore: tailwindcss config 파일 추가 및 global CSS 레이아웃에 import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Button from "@/components/button/Button";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <>
+      <div>hi</div>
+      <Button />
+    </>
+  );
 }
