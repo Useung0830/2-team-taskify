9ed56fc74a5a2961f93f20babf8a1425522f90ab
chaemin58 <sulv0309@naver.com>
2026-04-20 15:29:52 +0900

    feat: 비어있는 mydashboard UI 구현

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -1,3 +1,19 @@
+import Emptydashboard from "./components/Emptydashboard";
+
 export default function MyDashboard() {
-  return <div>MyDashboard</div>;
+  return (
+    <div className="bg-background text-gray-100 min-h-screen w-full">
+      <h1 className=" text-4xl pr-12.5 pl-12.5 pt-6 pb-4.5 ">홈</h1>
+      <div className=" text-xl m-12.5 mt-0">
+        내 대시보드
+        {/* 서버에서 불러온 데이터가 비어 있으면
+        empty 컴포넌트를 리턴하고 비어있지 않으면 데이터를 보여줌 */}
+        <Emptydashboard dashtype='my' />
+      </div>
+      <div className=" text-xl m-12.5 mt-0">
+        초대 받은 대시보드
+        <Emptydashboard dashtype='invite' />
+      </div>
+    </div>
+  );
 }
