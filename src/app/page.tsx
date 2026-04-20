dffd8e3bcefe054adce3237c0d2ff7516e05a1cb
chaemin58 <sulv0309@naver.com>
2026-04-20 16:18:37 +0900

    test: 메인 페이지에 테스트 코드 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Badge from "@/components/Badge";
+
 export default function Home() {
-  return <div>hi</div>;
+  // 확인을 위한 테스트 코드
+  // @TODO 나중에 수정 필요
+
+  return (
+  <>
+    <Badge color='green'>프로젝트</Badge>
+    <Badge color='blue'>상</Badge>
+    <Badge color='violet'>디자인</Badge>
+  </>
+  );
 }
