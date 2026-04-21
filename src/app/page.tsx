42dc773e172b3a6483caeb4a1a183af40472bc31
chaemin58 <sulv0309@naver.com>
2026-04-21 09:45:14 +0900

    fix: 메인페이지 테스트코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Badge from "@/components/Badge";
-
 export default function Home() {
-  // 확인을 위한 테스트 코드
-  // @TODO 나중에 수정 필요
-
-  return (
-  <>
-    <Badge color='green'>프로젝트</Badge>
-    <Badge color='blue'>상</Badge>
-    <Badge color='violet'>디자인</Badge>
-  </>
-  );
+  return <h1>hi</h1>;
 }
