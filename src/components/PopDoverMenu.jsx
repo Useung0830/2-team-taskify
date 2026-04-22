53a4db0ff0bbc716851e432b9cb22fd5c26ef640
Sung_yuseung <usngsung0830@naver.com>
2026-04-23 00:50:01 +0900

    refactor: PopDoverMenu 띄우는 위치 변경

--- src/components/PopDoverMenu.jsx
+++ src/components/PopDoverMenu.jsx
@@ -5,7 +5,7 @@ import icEdit from "@/assets/ic-edit.svg";
 
 export function PopDoverMenu() {
   return (
-    <div className="border-stroke absolute right-0 mt-2 w-35 rounded-xl border bg-[#2F2F33] shadow-lg">
+    <div className="border-stroke absolute top-5 right-0 mt-2 w-35 rounded-xl border bg-[#2F2F33] shadow-lg">
       <ul className="flex flex-col items-center justify-center text-sm text-gray-700">
         <li className="flex cursor-pointer items-center justify-center gap-2.5 rounded-t-xl px-5.5 pt-4 pb-2.5 transition-colors duration-300 hover:bg-gray-800">
           <Image className="h-5" src={icEdit} alt="수정 버튼" />
