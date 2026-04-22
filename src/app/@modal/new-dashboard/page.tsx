338eb9d79432a9dd5bf7aef4ef3a83fa5aa682d5
chaemin58 <sulv0309@naver.com>
2026-04-22 15:58:49 +0900

    feat: 간단한 레이아웃 틀

--- src/app/@modal/new-dashboard/page.tsx
+++ src/app/@modal/new-dashboard/page.tsx
@@ -10,5 +10,18 @@ export default function NewDashboard() {
     router.back();
   };
 
-  return <ModalHeader onClose={handleCloseModal}>새 대시보드 생성</ModalHeader>;
+  return (
+    <div className="border-gray-stroke flex flex-col gap-5 rounded-[24px]">
+      <ModalHeader onClose={handleCloseModal}>새 대시보드 생성</ModalHeader>
+      <input
+        className="bg-black-800 h-13.5 rounded-[14px] border border-gray-700 px-5 py-1.5 text-gray-400"
+        placeholder="새로운 대시보드"
+      />
+      <div></div>
+      <div className="flex gap-5">
+        <button>취소</button>
+        <button>생성</button>
+      </div>
+    </div>
+  );
 }
