c75e18c921ed80526475ecdf37add3f1a25e358b
chaemin58 <sulv0309@naver.com>
2026-04-23 03:03:41 +0900

    feat: 페이지네이션을 위한 로직 추가

--- src/app/mydashboard/components/MydashContainer.tsx
+++ src/app/mydashboard/components/MydashContainer.tsx
@@ -1,16 +1,63 @@
+"use client";
+
 import MydashboardList from "./MydashboardList";
 import rightbtn from "../assets/rightBtn.svg";
 import leftbtn from "../assets/leftBtn.svg";
 import Image from "next/image";
+import mockdata from "./mock";
+import { useState } from "react";
 
 export default function MydashContainer() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const SHOW_FIRST_ITEM = 3;
+  const SHOW_ITEMS = 4;
+
+  const getDataSlicing = () => {
+    if (currentPage === 0) {
+      return mockdata.slice(0, SHOW_FIRST_ITEM);
+    }
+
+    const start = SHOW_FIRST_ITEM + SHOW_ITEMS * (currentPage - 1);
+    const end = start + SHOW_ITEMS;
+
+    return mockdata.slice(start, end);
+  };
+
+  const totalPages =
+    Math.ceil((mockdata.length - SHOW_FIRST_ITEM) / SHOW_ITEMS) + 1;
+
+  const handleNext = () => {
+    if (currentPage < totalPages - 1) setCurrentPage((p) => p + 1);
+  };
+
+  const handlePrev = () => {
+    if (currentPage > 0) setCurrentPage((p) => p - 1);
+  };
+
+  const pagedData = getDataSlicing();
+
   return (
     <div className="flex flex-col py-2.5">
-      <MydashboardList />
+      <MydashboardList data={pagedData} currentPage={currentPage} />
       <div className="ml-auto flex gap-5 pt-5">
-        <div> 1 of 3</div>
-        <Image src={leftbtn} alt="left" />
-        <Image src={rightbtn} alt="right" />
+        <div>
+          {" "}
+          {currentPage} of {totalPages}
+        </div>
+        <Image
+          onClick={() => {
+            handlePrev;
+          }}
+          src={leftbtn}
+          alt="left"
+        />
+        <Image
+          onClick={() => {
+            handleNext;
+          }}
+          src={rightbtn}
+          alt="right"
+        />
       </div>
     </div>
   );
--- src/app/mydashboard/components/MydashboardList.tsx
+++ src/app/mydashboard/components/MydashboardList.tsx
@@ -1,24 +1,35 @@
+"use Client";
+
 import MyDashboardItemContainer from "./MyDashBoardItemContainer";
 import MyDashboardItemContainerAdd from "./MyDashboardItemContainerAdd";
 import tag from "../assets/colorchips.svg";
 import Image from "next/image";
+import { mockdataType } from "./mock";
 
-export default function MydashboardList() {
+interface MydashboardListProp {
+  data: mockdataType[];
+  currentPage: number;
+}
+
+export default function MydashboardList({
+  data,
+  currentPage,
+}: MydashboardListProp) {
   return (
     <div className="flex gap-5">
-      <MyDashboardItemContainerAdd />
+      {/* <MyDashboardItemContainerAdd />
       <MyDashboardItemContainer>
         <Image src={tag} alt="tag" />
         포트폴리오
-      </MyDashboardItemContainer>
-      <MyDashboardItemContainer>
-        <Image src={tag} alt="tag" />
-        포트폴리오
-      </MyDashboardItemContainer>
-      <MyDashboardItemContainer>
-        <Image src={tag} alt="tag" />
-        포트폴리오
-      </MyDashboardItemContainer>
+      </MyDashboardItemContainer> */}
+
+      {currentPage === 0 && <MyDashboardItemContainerAdd />}
+      {data.map((item: mockdataType) => (
+        <MyDashboardItemContainer>
+          <Image src={tag} alt="tag" />
+          {item.title}
+        </MyDashboardItemContainer>
+      ))}
     </div>
   );
 }
