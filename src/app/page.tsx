c71533efbbaa777c33992d3c67afddcc2389b405
Sung_yuseung <usngsung0830@naver.com>
2026-04-23 01:16:26 +0900

    refactor: 메인 페이지 비우기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Link from "next/link";
-
-import { Comment } from "@/components/Comment";
-import { CommentForm } from "@/components/CommentForm";
-
 export default function Home() {
-  return (
-    <>
-      <CommentForm />
-      <Comment />
-      <Link href={"./task-detail"}>알랄랄</Link>
-    </>
-  );
+  return <>hi</>;
 }
