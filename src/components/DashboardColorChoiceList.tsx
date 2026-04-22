6742ee4b79ee08c1e8371e93c137f4d3d04ecfb3
chaemin58 <sulv0309@naver.com>
2026-04-22 20:05:37 +0900

    fix: 테일윈드 클래스 적용안되는 문제 해결

--- src/components/DashboardColorChoiceList.tsx
+++ src/components/DashboardColorChoiceList.tsx
@@ -1,7 +1,7 @@
 import { DashboardColorItem } from "./DashboardColorItem";
 
 interface DashboardColorChoiceListProp {
-  height: number;
+  height: string;
 }
 
 export function DashboardColorChoiceList({
@@ -11,7 +11,7 @@ export function DashboardColorChoiceList({
   return (
     <div className="flex flex-col gap-3">
       <div className="text-gray-300">색상</div>
-      <div className={`flex h-[${height}px] gap-4`}>
+      <div className={`flex ${height} gap-4`}>
         {colorList.map((color) => (
           <DashboardColorItem key={color} color={color} />
         ))}
