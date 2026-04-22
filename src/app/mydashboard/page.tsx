4b89e8559e821f1f21babcfc1da078006988b600
chaemin58 <sulv0309@naver.com>
2026-04-22 13:12:52 +0900

    fix: data에 따라서 비어 있는 것과 데이터가 있는 UI를 보여줄 있도록 구조 변경

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -3,19 +3,25 @@ import InventionContainer from "./components/InventionContainer";
 import MydashContainer from "./components/MydashContainer";
 
 export default function MyDashboard() {
-  const data = false;
+  const mydata = true;
+  const invitedata = true;
+
   return (
-    <div className="font-pretendard min-h-screen w-full text-gray-100">
-      <h1 className="pt-6 pr-12.5 pb-4.5 pl-12.5 text-4xl">홈</h1>
-      <div className="m-12.5 mt-0 text-xl">
-        내 대시보드
+    <div className="font-pretendard flex min-h-screen w-full flex-col text-gray-100">
+      <h1 className="px-12.5 pt-6 pb-3.5 text-4xl font-bold">홈</h1>
+      <div className="flex flex-col gap-5 px-12.5 pb-12.5 text-xl">
+        <div className="py-1">내 대시보드</div>
         {/* 서버에서 불러온 데이터가 비어 있으면
         empty 컴포넌트를 리턴하고 비어있지 않으면 데이터를 보여줌 */}
-        {data ? <Emptydashboard dashtype="my" /> : <MydashContainer />}
+        {mydata ? <MydashContainer /> : <Emptydashboard dashtype="my" />}
       </div>
-      <div className="m-12.5 mt-0 text-xl">
-        초대 받은 대시보드
-        {data ? <Emptydashboard dashtype="invite" /> : <InventionContainer />}
+      <div className="flex flex-col gap-5 px-12.5 pb-12.5 text-xl">
+        <div className="py-1">초대 받은 대시보드</div>
+        {invitedata ? (
+          <InventionContainer />
+        ) : (
+          <Emptydashboard dashtype="invite" />
+        )}
       </div>
     </div>
   );
