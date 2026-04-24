34af84f3825b58d21dfe20cce1d2d228850ef864
Sung_yuseung <usngsung0830@naver.com>
2026-04-24 20:49:48 +0900

    refactor: new-Dashboard page 수정

--- src/app/@modal/new-dashboard/page.tsx
+++ src/app/@modal/new-dashboard/page.tsx
@@ -1,20 +1,10 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-
 import { DashboardColorChoiceList } from "@/components/DashboardColorChoiceList";
 import { ModalHeader } from "@/components/ModalHeader";
 
 export default function NewDashboard() {
-  const router = useRouter();
-
-  const handleCloseModal = () => {
-    router.back();
-  };
-
   return (
     <div className="border-gray-stroke flex flex-col gap-5 rounded-3xl">
-      <ModalHeader onClose={handleCloseModal}>새 대시보드 생성</ModalHeader>
+      <ModalHeader>새 대시보드 생성</ModalHeader>
       <input
         className="bg-black-800 h-13.5 rounded-[14px] border border-gray-700 px-5 py-1.5 text-gray-400"
         placeholder="새로운 대시보드"
