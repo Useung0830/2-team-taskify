bd32b7044610fde789ccaa8561209efd47a703a5
imyoonsoo <bliss38soo@gmail.com>
2026-04-24 20:00:41 +0900

    fix/refactor: PR 코멘트 반영하여 DeleteAlertModal.tsx, Modal.tsx, column-delte/page.tsx 코드 수정

--- src/app/@modal/column-delete/page.tsx
+++ src/app/@modal/column-delete/page.tsx
@@ -1,22 +1,9 @@
-"use client";
-
-import { useState } from "react";
-
 import { DeleteAlertModal } from "@/components/DeleteAlertModal";
 
 export default function ColumnDelete() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => setIsOpen(false);
-  const handleDelete = () => {
-    setIsOpen(false);
-  };
-
   return (
-    <DeleteAlertModal
-      isOpen={isOpen}
-      onClose={handleClose}
-      onDelete={handleDelete}
-    />
+    <div>
+      <DeleteAlertModal />;
+    </div>
   );
 }
--- src/components/DeleteAlertModal.tsx
+++ src/components/DeleteAlertModal.tsx
@@ -1,70 +1,61 @@
-/**
- * DeleteAlertModal 코멘트 모두 resolve conversation 해서 올리는 2차 PR
- */
-
 "use client";
+
+import { useRouter } from "next/navigation";
+
+import ModalLayout from "@/app/@modal/layout";
 import { cn } from "@/lib/cn";
 
 import { Button } from "./Button";
-import { Modal } from "./Modal";
+export function DeleteAlertModal() {
+  const router = useRouter();
 
-interface DeleteAlertModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onDelete: () => void;
-}
+  // 취소 버튼 클릭 시 router.back해서 url이동으로 모달을 닫음
+  const handleClose = () => router.back();
 
-export function DeleteAlertModal({
-  isOpen,
-  onClose,
-  onDelete,
-}: DeleteAlertModalProps) {
-  if (!isOpen) return null;
-
-  const handleClose = () => {
-    onClose();
-  };
-
-  const handleDelete = () => {
-    onDelete();
-    onClose();
+  // 삭제 버튼 클릭 시 router.back해서 url이동으로 모달을 닫고, 데이터 갱신
+  const handleDelete = async () => {
+    try {
+      console.warn("삭제 API 호출");
+      router.back();
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <Modal>
+    <ModalLayout>
       <div
         className={cn(
-          "mx-auto flex flex-col items-center justify-center text-center",
-          "max-w-150 min-w-83.75",
-          "h-auto",
-          "gap-6 py-2 pt-2 md:gap-8 md:pt-4"
+          "flex flex-col items-center justify-center text-center",
+          "w-full gap-8 md:gap-10" // 상자 안에서의 여백만 설정
         )}
       >
-        <div className="flex w-full flex-col items-center gap-2 text-center md:gap-3">
-          <h2 className="text-lg font-semibold whitespace-nowrap text-gray-200 md:text-2xl">
+        <div className="flex w-full flex-col items-center gap-2 md:gap-3">
+          <h2 className="text-lg font-semibold text-gray-200 lg:text-xl">
             칼럼을 삭제하시겠습니까?
           </h2>
-          <p className="text-base whitespace-nowrap text-gray-400 md:text-xl">
+          <p className="text-base font-semibold text-gray-400 lg:text-lg">
             칼럼 내 모든 카드도 함께 삭제됩니다.
           </p>
         </div>
 
-        <div className="flex w-full gap-3 md:gap-5">
+        <div className="flex w-full items-center justify-center gap-3 md:gap-5">
           <Button
             colortype="secondary"
-            className="h-12.5 flex-1 items-center justify-center text-base text-gray-100 md:h-15 md:text-lg"
+            className="h-15 flex-1 text-base text-gray-100 lg:h-12.5 lg:text-lg"
             onClick={handleClose}
           >
             취소
           </Button>
           <Button
-            className="bg-red! h-12.5 flex-1 items-center justify-center text-base text-white md:h-15 md:text-lg"
+            className="bg-profile-rose h-15 flex-1 text-base text-white hover:bg-red-800 active:bg-red-900 lg:h-12.5 lg:text-lg"
             onClick={handleDelete}
           >
             삭제
           </Button>
         </div>
       </div>
-    </Modal>
+    </ModalLayout>
   );
 }
