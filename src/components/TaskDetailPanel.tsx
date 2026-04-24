ad0431107b5b36441ec38991db9643721e4c7fbd
Sung_yuseung <usngsung0830@naver.com>
2026-04-24 16:24:08 +0900

    style: lg화면 크기일 때 TaskDetilPenel items-center 추가

--- src/components/TaskDetailPanel.tsx
+++ src/components/TaskDetailPanel.tsx
@@ -8,17 +8,17 @@ export function TaskDetailPanel() {
         <KebabButton />
         <ModalCloseButton />
       </div>
-      <div className="max-lg:border-black-600 flex flex-col gap-2.5 border-b border-[#696773] pb-3 max-lg:w-full max-lg:flex-row">
+      <div className="max-lg:border-black-600 flex flex-col gap-2.5 border-b border-[#696773] pb-3 max-lg:w-full max-lg:flex-row max-lg:items-center">
         <span className="text-sm text-gray-300">프로젝트</span>
         <span className="text-basic text-gray-100">
           포트폴리오 / On Progress
         </span>
       </div>
-      <div className="max-lg:border-black-600 flex flex-col gap-2.5 border-b border-[#696773] pt-2.5 pb-3 max-lg:flex-row">
+      <div className="max-lg:border-black-600 flex flex-col gap-2.5 border-b border-[#696773] pt-2.5 pb-3 max-lg:flex-row max-lg:items-center">
         <span className="text-sm text-gray-300">담당자</span>
         <span className="text-basic text-gray-100">박민영</span>
       </div>
-      <div className="flex flex-col gap-2.5 pt-2.5 max-lg:flex-row">
+      <div className="flex flex-col gap-2.5 pt-2.5 max-lg:flex-row max-lg:items-center">
         <span className="text-sm text-gray-300">마감일</span>
         <span className="text-basic text-gray-100">2025년 7월 17일</span>
       </div>
