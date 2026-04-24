ff9e6f06d4d7c53e35f3f2498ddff2232d1cc338
chaemin58 <sulv0309@naver.com>
2026-04-25 01:56:47 +0900

    fix: 쓸데없는 prop 삭제

--- src/app/@modal/new-dashboard/page.tsx
+++ src/app/@modal/new-dashboard/page.tsx
@@ -13,21 +13,18 @@ export default function NewDashboard() {
   };
 
   return (
-    <div className="border-gray-stroke flex flex-col gap-4 rounded-3xl md:gap-5">
+    <div className="border-gray-stroke flex flex-col gap-5 rounded-3xl">
       <ModalHeader onClose={handleCloseModal}>새 대시보드 생성</ModalHeader>
       <input
         className="bg-black-800 h-13.5 rounded-[14px] border border-gray-700 px-5 py-1.5 text-gray-400"
         placeholder="새로운 대시보드"
       />
-      <DashboardColorChoiceList
-        size={"sm"}
-        responsiveSize="md:w-[540px] md:h-[60px]"
-      />
+      <DashboardColorChoiceList />
       <div className="flex gap-5">
-        <button className="h-9.5 w-full rounded-[100px] bg-gray-900 px-7.5 py-1.5 md:h-12">
+        <button className="h-15 w-full rounded-[100px] bg-gray-900 px-7.5 py-1.5">
           취소
         </button>
-        <button className="h-9.5 w-full rounded-[100px] bg-green-500 px-7.5 py-1.5 md:h-12">
+        <button className="h-15 w-full rounded-[100px] bg-green-500 px-7.5 py-1.5">
           생성
         </button>
       </div>
--- src/components/DashboardColorChoiceList.tsx
+++ src/components/DashboardColorChoiceList.tsx
@@ -1,17 +1,13 @@
 import { cva, VariantProps } from "class-variance-authority";
-import { twMerge } from "tailwind-merge";
 
 import { DashboardColorItem } from "./DashboardColorItem";
 
-type DashboardColorChoiceListProp = VariantProps<typeof sizeVariants> & {
-  responsiveSize?: string;
-};
+type DashboardColorChoiceListProp = VariantProps<typeof sizeVariants>;
 
 const sizeVariants = cva("flex gap-2", {
   variants: {
     size: {
-      default: "h-15 w-135",
-      sm: "h-10 w-73.75",
+      default: "h-10 w-73.75 md:h-15 md:w-135",
       lg: "h-22.5 w-185",
     },
   },
@@ -23,13 +19,12 @@ const sizeVariants = cva("flex gap-2", {
 
 export function DashboardColorChoiceList({
   size,
-  responsiveSize,
 }: DashboardColorChoiceListProp) {
   const colorList = ["red", "orange", "yellow", "green", "blue"] as const;
   return (
     <div className="flex flex-col gap-3">
       <div className="text-gray-300">색상</div>
-      <div className={twMerge(sizeVariants({ size }), responsiveSize)}>
+      <div className={sizeVariants({ size })}>
         {colorList.map((color) => (
           <DashboardColorItem key={color} color={color} />
         ))}
