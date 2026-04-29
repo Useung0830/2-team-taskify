481beeaafa1bffef223429b4453a9e5983cb81eb
Evo1ver <Evo1ver@naver.com>
2026-04-29 17:41:27 +0900

    feat: Badge 관련 컴포넌트 수정 - Badge 이름에 따라 랜덤한 색상을 가지도록 설정   - 이 때 결정되는 색상은 Badge 배경 및 Badge 글자 색상 - BadgeContainer를 별도로 분리

--- src/app/dashboard/[id]/_components/ColumnCard.tsx
+++ src/app/dashboard/[id]/_components/ColumnCard.tsx
@@ -1,6 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 
-import { Badge } from "@/components/Badge";
+import { BadgeContainer } from "@/components/Badge/BadgeContainer";
 
 import { Profile } from "./Profile";
 
@@ -26,11 +26,8 @@ export function ColumnCard({
     >
       {imgSrc && <Image className="w-full" src={imgSrc} alt="userAddImg" />}
       <h1 className="text-[18px] font-semibold">{cardTitle}</h1>
-      <div className="flex gap-1.5">
-        {/* 기능구현할 때는 배지리스트 받아서 처리 */}
-        <Badge color="blue">프로젝트</Badge>
-        <Badge color="green">상</Badge>
-      </div>
+      {/* 기능구현할 때는 배지리스트 받아서 처리 */}
+      <BadgeContainer tags={["프로젝트", "상"]} />
       <div>2025년 7월 20일</div>
       <Profile name={creator} />
     </div>
