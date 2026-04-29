255b91bbc925b3e4a7b9c12fee25211b7ffb8a09
chaemin58 <sulv0309@naver.com>
2026-04-29 12:33:39 +0900

    feat: 새로운 대시보드 누르면 대시보드 생성 모달로 이동

--- src/app/mydashboard/_components/MyDashBoardItemContainer.tsx
+++ src/app/mydashboard/_components/MyDashBoardItemContainer.tsx
@@ -13,13 +13,12 @@ export function MyDashboardItemContainer({
   dashid,
 }: MyDashboardCardProps) {
   return (
-    <div
-      className={`bg-black-700 flex h-20 w-full items-center justify-between rounded-3xl border-2 border-gray-700 px-5 py-3`}
+    <Link
+      href={`/dashboard/${dashid}`}
+      className="bg-black-700 flex h-20 w-full items-center justify-between gap-1 rounded-3xl border-2 border-gray-700 px-5 py-3"
     >
       {children}
-      <Link href={`/dashboard/${dashid}`} className="flex gap-1">
-        <Image src={rightbtn} alt="right" className="ml-auto" />
-      </Link>
-    </div>
+      <Image src={rightbtn} alt="right" className="ml-auto" />
+    </Link>
   );
 }
