a7fc6aceb47fb075cb50296a80883ca1135ab51d
chaemin58 <sulv0309@naver.com>
2026-04-30 14:52:03 +0900

    feat: 대시보드 생성 기능 추가

--- src/app/@modal/new-dashboard/page.tsx
+++ src/app/@modal/new-dashboard/page.tsx
@@ -1,23 +1,94 @@
-import { DashboardColorChoiceList } from "@/components/DashboardColorChoiceList";
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { postDashboard } from "@/api/data";
+import { Button } from "@/components/Button";
+import {
+  ColorName,
+  DashboardColorChoiceList,
+} from "@/components/DashboardColorChoiceList";
+import { Input } from "@/components/input/input";
 import { ModalHeader } from "@/components/modal/ModalHeader";
 
 export default function NewDashboard() {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [selectColor, setSelectColor] = useState();
+  const [selectHex, setSelectHex] = useState("");
+  const [hasSelection, setHasSelection] = useState<boolean>(false);
+  const router = useRouter();
+
+  const ColorMath = {
+    red: "#ae2e24",
+    orange: "#9f4b00",
+    yellow: "#bd8c00",
+    green: "#206e4e",
+    blue: "#1458bc",
+  };
+
+  const handleFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (error) setError("");
+  };
+
+  // const handleFieldBlur = () => {
+  //   if (value.length < 3) {
+  //     setError("검색 글자 수는 3글자 이상이어야 합니다.");
+  //   }
+  // };
+
+  const handleColorSelect = (id: ColorName) => {
+    const hexcode = typeToHex(id);
+    setSelectHex(hexcode);
+    setHasSelection(true);
+  };
+
+  const postData = {
+    title: value,
+    color: selectHex,
+  };
+
+  //selectColor를 헥사코드로 바꿔주는 함수
+  const typeToHex = (type: ColorName) => {
+    return ColorMath[type];
+  };
+
+  //대시보드 생성하는 함수
+  const handlePostNewDash = async () => {
+    if (selectColor && value) {
+      const response = await postDashboard(postData);
+      router.push(`/dashboard/${response.id}`);
+    }
+  };
+
   return (
     <div className="border-gray-stroke flex flex-col gap-5 rounded-3xl">
       <ModalHeader>새 대시보드 생성</ModalHeader>
-      <input
-        className="bg-black-800 h-13.5 rounded-md border border-gray-700 px-5 py-1.5 text-gray-400"
-        placeholder="새로운 대시보드"
-      />
-      <DashboardColorChoiceList />
-      <div className="flex gap-5">
-        <button className="h-15 w-full rounded-full bg-gray-900 px-7.5 py-1.5">
-          취소
-        </button>
-        <button className="h-15 w-full rounded-full bg-green-500 px-7.5 py-1.5">
-          생성
-        </button>
-      </div>
+      <form onSubmit={handlePostNewDash} className="flex flex-col gap-5">
+        <Input>
+          <Input.Wrapper>
+            <Input.Field
+              id="test"
+              placeholder="대시보드 이름을 입력해주세요."
+              value={value}
+              onChange={handleFieldChange}
+              // onBlur={handleFieldBlur}
+            />
+          </Input.Wrapper>
+          <Input.Error />
+        </Input>
+        <DashboardColorChoiceList
+          onColorChange={handleColorSelect}
+          selectedColorName={selectColor}
+          hasSelection={hasSelection}
+        />
+        <div className="flex gap-5">
+          <Button colorType="secondary">취소</Button>
+          <Button>생성</Button>
+        </div>
+      </form>
     </div>
   );
 }
