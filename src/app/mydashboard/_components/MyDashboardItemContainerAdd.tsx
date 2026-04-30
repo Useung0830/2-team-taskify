99caa247cf5b7c23db74855a6b5a2ba4be2415fe
chaemin58 <sulv0309@naver.com>
2026-04-30 16:49:31 +0900

    fix: 새로운 대시보드 디자인 수정

--- src/app/mydashboard/_components/MyDashboardItemContainerAdd.tsx
+++ src/app/mydashboard/_components/MyDashboardItemContainerAdd.tsx
@@ -5,13 +5,12 @@ import addBtn from "@/assets/mydashboard/mydashboardAddBtn.svg";
 
 export function MyDashboardItemContainerAdd() {
   return (
-    <Link href={"/new-dashboard"}>
-      <div
-        className={`bg-black-800 flex h-20 w-full items-center justify-center gap-2.5 rounded-3xl border-2 border-dashed border-gray-700 px-5 py-3`}
-      >
-        새로운 대시보드
-        <Image src={addBtn} alt="add" />
-      </div>
+    <Link
+      href={"/new-dashboard"}
+      className="bg-black-800 flex h-20 w-full items-center justify-center gap-2.5 rounded-3xl border-2 border-dashed border-gray-700 px-5 py-3"
+    >
+      새로운 대시보드
+      <Image src={addBtn} alt="add" />
     </Link>
   );
 }
