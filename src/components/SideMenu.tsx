fbe91e8f6ab8c18a59cfdce04c6a7403fd90755c
Sung_yuseung <usngsung0830@naver.com>
2026-04-30 11:47:43 +0900

    feat: sidemenu 버튼 링크 연결

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+import logoImg from "@/assets/common/logo.svg";
+
 import { DashboardAdd } from "./DashboardAdd";
 import { SideButton } from "./SideButton";
 import { SideHomeButton } from "./SideHomeButton";
@@ -7,18 +11,14 @@ export function SideMenu() {
   return (
     <div className="bg-black-900 flex h-screen w-85 flex-col pt-2.5">
       <div className="flex h-11 items-start px-4 text-2xl text-gray-100">
-        Logo
+        <Image src={logoImg} height={40} width={156} alt="로고 이미지" />
       </div>
       <div className="flex w-85 flex-1 flex-col justify-start px-6">
         <div className="flex flex-col">
           <DashboardAdd />
           <SideHomeButton />
-          <div className="flex flex-col text-gray-100">
+          <div className="flex flex-col overflow-y-auto text-gray-100">
             <SideButton />
-            <div>사이드 버튼</div>
-            <div>사이드 버튼</div>
-            <div>사이드 버튼</div>
-            <div>사이드 버튼</div>
           </div>
         </div>
       </div>
